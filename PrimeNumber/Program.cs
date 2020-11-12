b71d9f4f11b5a4558bc3cf6e3ac9db1a68177e14
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-11-12 23:39:34 +0200

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int divider = 2;
+
+            while (x % divider != 0)
+            {
+                divider++;
+            }
+
+            if (divider == x)
+            {
+                Console.WriteLine("Is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("Is not a prime number");
+            }
+        }
+    }
+}
